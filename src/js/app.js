22474091a4c3d82852bf8e4af211e05c5760031a
Szymon Rocznik <szymonrocznik@gmail.com>
2025-03-17 14:43:30 +0100

    faq page

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,45 @@
-const testModules = require('./test-module');
-require('../css/app.css');
-require('../scss/style.scss');
+
 
 /********** Paste your code here! ************/
+$(document).ready(function() {
+  let isAnimating = false;
 
-console.log('Paste your code here!');
-console.log(testModules.hello);
+  $('.panel-title a').on('click', function(event) {
+      if (isAnimating) {
+          event.preventDefault();
+          return;
+      }
+
+      isAnimating = true;
+      setTimeout(() => {
+          isAnimating = false;
+      }, 10000);
+
+      $('.panel-heading').removeClass('active');
+      $(this).closest('.panel-heading').addClass('active');
+  });
+
+  $('#accordion').on('show.bs.collapse', function(e) {
+      let panel = $(e.target).closest('.panel');
+      panel.find('.panel-heading').addClass('active');
+      panel.addClass('active');
+      panel.find('img').attr('src', 'images/accordion_click.png'); // Zmiana obrazu po otwarciu
+  });
+
+  $('#accordion').on('hide.bs.collapse', function(e) {
+      let panel = $(e.target).closest('.panel');
+      panel.find('.panel-heading').removeClass('active');
+      panel.removeClass('active');
+      panel.find('img').attr('src', 'images/accordion.png'); // Przywrócenie obrazu po zamknięciu
+  });
+
+  // Zmiana obrazu pliku na hover
+  $('.files p img').hover(
+      function() {
+          $(this).attr('src', 'images/file_hover.png'); // Zmiana obrazu na hover
+      }, 
+      function() {
+          $(this).attr('src', 'images/file.png'); // Powrót do normalnego obrazu
+      }
+  );
+});
